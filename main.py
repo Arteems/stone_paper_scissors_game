e20d0499406e9b2fe166ad8b8d158cbb4c023bcc
Arteems <artm_frolov00@mail.ru>
2024-09-06 18:15:36 +0200

    create main file and add gitignore

---
+++ main.py
@@ -0,0 +1,28 @@
+from typing import Literal
+
+from fastapi import FastAPI
+import random
+
+
+app = FastAPI()
+
+
+def computer_choice() -> str:
+    choices = ["Камень", "Ножницы", "Бумага"]
+    return random.choice(choices)
+
+
+@app.get("/choice")
+def user_choice(user: Literal["Камень", "Ножницы", "Бумага"]):
+    computer = computer_choice()
+
+    if user == computer:
+        return f"Ничья. Компьютер выбрал {computer}"
+    elif (
+        (user == "Камень" and computer == "Ножницы")
+        or (user == "Бумага" and computer == "Камень")
+        or (user == "Ножницы" and computer == "Бумага")
+    ):
+        return f"Вы выиграли! Компьютер выбрал {computer}"
+
+    return f"Вы проиграли:( Компьютер выбрал {computer}"
